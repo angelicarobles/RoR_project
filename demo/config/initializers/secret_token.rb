a27d6d63f4a5065c7ac0258000514bc66ed04982
Angelica Robles <angelica.robles@longboat.com>
2014-12-16 16:32:42 +0000

    Adding new RoR project

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '64ce3ffff652e88304d76209c6f8100a3ba45c35dfb45cd0b0d2afea32cced44e609255a52975fc15db76978dd91c356d8fd8362e9e3f0e959ae036df0f9458f'
